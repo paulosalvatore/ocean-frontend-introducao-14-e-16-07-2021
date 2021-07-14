e6be690bc10993fb67ce14e422aaadfccc7444ee
Paulo Salvatore <salvatoregames@gmail.com>
2021-07-14 12:17:35 -0300

    Criação dos componentes App, Lista e Item

--- src/index.js
+++ src/index.js
@@ -1,17 +1,56 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+/*
+CRUD (Create, Read (Single & All), Update, Delete)
+
+Create -> Formulário de criação
+Read All -> Listagem de itens cadastrados
+Read Single -> Visualização de um item específico
+Update -> Formulário de edição
+Delete -> Remover um item específico
+*/
+
+// Read All
+// Precisa dos dados que serão exibidos
+// A estrutura desses dados precisa possuir 'Nome' e 'URL da Imagem'
+
+// Decidam qual será o tema da sua aplicação
+// O meu será de Raças de Cachorro
+
+const lista = [
+    {
+        id: 1,
+        nome: "Golden Retriever",
+        imagemUrl:
+            "https://f.i.uol.com.br/fotografia/2017/10/27/150912457859f369e28eaa5_1509124578_3x2_md.jpg",
+    },
+];
+
+function Item() {
+    return <div>Item</div>;
+}
+
+function Lista() {
+    return (
+        <div>
+            <Item />
+        </div>
+    );
+}
+
+function App() {
+    return (
+        <div>
+            <Lista />
+        </div>
+    );
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
