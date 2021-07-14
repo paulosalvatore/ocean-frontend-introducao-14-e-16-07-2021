5ca7ae2fae02d4329f7ab0ddf92035e2235b7a6d
Paulo Salvatore <salvatoregames@gmail.com>
2021-07-14 12:30:42 -0300

    Exibindo nome da lista e novo item na lista

--- src/index.js
+++ src/index.js
@@ -26,15 +26,24 @@ const lista = [
         imagemUrl:
             "https://f.i.uol.com.br/fotografia/2017/10/27/150912457859f369e28eaa5_1509124578_3x2_md.jpg",
     },
+    {
+        id: 2,
+        nome: "Labrador",
+        imagemUrl:
+            "https://www.clubeparacachorros.com.br/wp-content/uploads/2014/07/labrador-amarelo.jpg",
+    },
 ];
 
 function Item() {
-    return <div>Item</div>;
+    const item = lista[0];
+
+    return <div>{item.nome}</div>;
 }
 
 function Lista() {
     return (
         <div>
+            <Item />
             <Item />
         </div>
     );
