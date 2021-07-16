2e85ee100df428ad04bf313d09dd5e90437190c6
Paulo Salvatore <salvatoregames@gmail.com>
2021-07-16 10:45:30 -0300

    Header e Footer adicionados

--- src/index.js
+++ src/index.js
@@ -58,10 +58,20 @@ function Lista() {
     );
 }
 
+function Header() {
+    return <div>Header</div>;
+}
+
+function Footer() {
+    return <div>Footer</div>;
+}
+
 function App() {
     return (
         <div>
+            <Header />
             <Lista />
+            <Footer />
         </div>
     );
 }
